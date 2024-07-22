05c04b71613acd40fd7ebec4a2f580f187575942
Vasily Myazin <vm@konspiredesign.com>
2024-07-22 20:15:48 -0300

    Add logo; extend layout

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow container mx-auto px-4 py-8">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-5xl mx-auto">
           <div className='flex w-full justify-space-between'>
             <div className='flex flex-col flex-grow'>
               <h1 className="text-4xl font-bold mb-2 text-gray-100">Vasily Simon</h1>
